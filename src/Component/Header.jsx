7b73ae5cb97de7882d8da37f79aeff4465765c83
Satyam799 <satyamtripathisvp@gmail.com>
2025-03-23 22:31:43 +0530

    new3

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,26 +2,32 @@ import { useState } from "react";
 import "./Header.css";
 
 function Header() {
-
-    const [dropdwon1,setdropdown1]=useState(false)
-    const [dropdown2,setdropdown2]=useState(false)
-
+  const [dropdwon1, setdropdown1] = useState(false);
+  const [dropdown2, setdropdown2] = useState(false);
 
   return (
     <div className="Heaader">
-      <div className="dropdown1" onMouseEnter={()=>setdropdown1(!dropdwon1)} onMouseLeave={()=>setdropdown1(!dropdwon1)}>
-        <p >Dropdwon1</p>
-        <div className={`dropdown2 ${dropdwon1 ? '' :'active1'}`}>
-          <p>option1</p>
-          <p>option2</p>
-          <p className="option3" onMouseEnter={()=>setdropdown2(!dropdown2)} onMouseLeave={()=>setdropdown2(!dropdown2)}>
-            option3
-            <div className={`dropdown3  ${dropdown2 ? '' :'active2'}`}>
-              <p>option1</p>
-              <p>option2</p>
-              <p>option3</p>
-            </div>
-          </p>
+      <div className="management">
+        <div className="dropdown1"
+        onMouseEnter={() => setdropdown1(!dropdwon1)}
+        onMouseLeave={() => setdropdown1(!dropdwon1)}>
+          <p>Dropdwon1</p>
+          <div className={`dropdown2 ${dropdwon1 ? "" : "active1"}`}>
+            <p>option1</p>
+            <p>option2</p>
+            <p
+              className="option3"
+              onMouseEnter={() => setdropdown2(!dropdown2)}
+              onMouseLeave={() => setdropdown2(!dropdown2)}
+            >
+              option3
+              <div className={`dropdown3  ${dropdown2 ? "" : "active2"}`}>
+                <p>option1</p>
+                <p>option2</p>
+                <p>option3</p>
+              </div>
+            </p>
+          </div>
         </div>
       </div>
     </div>
